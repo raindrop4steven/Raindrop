796ad713c8057947e8eba640c10b29130c810a56
steven <rockboyfor@gmail.com>
2016-01-02 13:41:30 +0800

    Update cocoapods and its libraries

---
+++ Raindrop/Controllers/Common/Login/RDPLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  RDPLoginViewController.h
+//  Raindrop
+//
+//  Created by user on 16/1/2.
+//  Copyright © 2016年 steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDPLoginViewController : UIViewController
+
+@end
---
+++ Raindrop/Controllers/Common/Login/RDPRegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RDPRegisterViewController.h
+//  Raindrop
+//
+//  Created by user on 16/1/2.
+//  Copyright © 2016年 steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDPRegisterViewController : UIViewController
+
+@end
